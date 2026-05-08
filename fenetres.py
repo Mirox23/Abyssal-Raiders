5a96889533e89915670b2599b6aa7d99e4d5ef04
Mateo <mateo.gerard.2009@gmail.com>
2026-05-08 22:50:35 +0200

    Fin des ajouts des docstring du système des fenpetres

--- fenetres.py
+++ fenetres.py
@@ -1,3 +1,9 @@
+"""
+Qu'est-ce que le fichier gère : Ce fichier gère la partie fenetres du projet.
+Entrée : Les données nécessaires aux fonctions, classes et paramètres du module.
+Résultat : Des comportements, calculs ou affichages utilisés par le jeu.
+"""
+
 from fenetre_infos_tours import PanneauInfos
 from fenetre_achevements import PanneauAchevement
 from fenetre_fin_vague import EcranFinVague

36b5ca3624a786ac2d566573ad3ff5033c9c0388
Mateo <mateo.gerard.2009@gmail.com>
2026-05-08 20:31:29 +0200

    Ajout des docstrings + correction du code

--- sauvegarde_dossier_parties.py
+++ sauvegarde_dossier_parties.py
@@ -5,21 +5,6 @@ DOSSIER_SAUVEGARDES = "sauvegardes"
 
 
 def assurer_le_dossier():
-    if not os.path.exists(DOSSIER_SAUVEGARDES):
-        os.makedirs(DOSSIER_SAUVEGARDES, exist_ok=True)
-"""
-Qu'est-ce que le fichier gère : Ce fichier gère la partie sauvegarde dossier parties du projet.
-Entrée : Les données nécessaires aux fonctions, classes et paramètres du module.
-Résultat : Des comportements, calculs ou affichages utilisés par le jeu.
-"""
-
-import os
-
-
-DOSSIER_SAUVEGARDES = "sauvegardes"
-
-
-def assurer_le_dossier():
     """
     Explication de ce que fais la fonction : Cette fonction exécute assurer le dossier.
     Les entrées : Cette fonction ne demande pas de paramètre direct.
@@ -27,3 +12,6 @@ def assurer_le_dossier():
     """
     if not os.path.exists(DOSSIER_SAUVEGARDES):
         os.makedirs(DOSSIER_SAUVEGARDES, exist_ok=True)
+
+
+
